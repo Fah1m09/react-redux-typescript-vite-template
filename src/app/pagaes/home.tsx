9955f5d02c9bdf0a39558a548768931c9d8523b8
Mahmudur Rahman <Mahmudur@pixelmask.com>
2022-09-22 19:39:52 +0600

    template initial push

---
+++ src/app/pagaes/home.tsx
@@ -0,0 +1,13 @@
+import { Grid } from "@mui/material";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+
+export default function HomePage() {
+  return (
+    <>
+      <Grid className="container">
+        <p>Hellow World I am here</p>
+      </Grid>
+    </>
+  );
+}
